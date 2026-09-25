541250cdc1e03f125d9ed0f0ccd20814c26516e0
Monti215 <maksim86yartsev86@gmail.com>
2026-09-25 21:06:36 +0500

    Правки

--- cloud-terraform/vms.tf
+++ cloud-terraform/vms.tf
@@ -123,5 +123,72 @@ resource "local_file" "inventory" {
   filename = "./hosts.ini"
 }
 
+resource "yandex_compute_instance" "elastic" {
+  name        = "el" #Имя ВМ в облачной консоли
+  hostname    = "el" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a" #зона ВМ должна совпадать с зоной subnet!!!
 
 
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = false }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_a.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.web_sg.id]
+  }
+}
+
+resource "yandex_compute_instance" "kibana" {
+  name        = "kibana" #Имя ВМ в облачной консоли
+  hostname    = "kibana" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a" #зона ВМ должна совпадать с зоной subnet!!!
+
+
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_2204_lts.image_id
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+
+  metadata = {
+    user-data          = file("./cloud-init.yml")
+    serial-port-enable = 1
+  }
+
+  scheduling_policy { preemptible = false }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop_a.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.LAN.id, yandex_vpc_security_group.web_sg.id]
+  }
+}
